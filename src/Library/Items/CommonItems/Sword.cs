07e27a5f4666a0c30c817a0afd264956d84c5b3d
Leandro Alfonso <leamanya24.6@gmail.com>
2020-12-17 21:53:44 -0300

    :tada: finalicé con la implementación de los items compuestos/combinados

--- src/Library/Items/CommonItems/Sword.cs
+++ src/Library/Items/CommonItems/Sword.cs
@@ -10,5 +10,34 @@
         {
             return "Sword";
         }
+        
+        //Combinations: sword+sword, sword+shield, sword+sharp shield
+
+        public CompoundNonMagicItem Combine(Sword sword)
+        {
+            return new CompoundNonMagicItem(
+                0,
+                this.DamageValue+sword.DamageValue,
+                0,
+                "Crossed swords");
+        }    
+        
+        public CompoundNonMagicItem Combine(Shield shield)
+        {
+            return new CompoundNonMagicItem(
+                shield.DefenseValue,
+                this.DamageValue,
+                0,
+                "Sword and shield");
+        }    
+        
+        public CompoundNonMagicItem Combine(SharpShield shield)
+        {
+            return new CompoundNonMagicItem(
+                shield.DefenseValue,
+                this.DamageValue+shield.DamageValue,
+                0,
+                "Sword and sharp shield");
+        }
     }
 }
--- src/Library/Items/CommonItems/Trident.cs
+++ src/Library/Items/CommonItems/Trident.cs
@@ -10,5 +10,27 @@
         {
             return "Trident";
         }
+        
+          
+        //Combinations: trident+shield, trident+sharp shield
+
+        
+        public CompoundNonMagicItem Combine(Shield shield)
+        {
+            return new CompoundNonMagicItem(
+                this.DefenseValue+shield.DefenseValue,
+                this.DamageValue+shield.DamageValue,
+                0,
+                "Trident and shield");
+        }    
+        
+        public CompoundNonMagicItem Combine(SharpShield shield)
+        {
+            return new CompoundNonMagicItem(
+                this.DefenseValue+shield.DefenseValue,
+                this.DamageValue+shield.DamageValue,
+                0,
+                "Trident and sharp shield");
+        }
     }
 }
